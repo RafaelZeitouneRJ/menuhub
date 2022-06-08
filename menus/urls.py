ef04000fcd960b6651dfc5e05057306b5420113b
Rafael Seiceira Zeitoune <rafael.zeitoune@portosrio.gov.br>
2022-06-08 16:51:03 -0300

    Atualização

--- menus/urls.py
+++ menus/urls.py
@@ -1,18 +1,17 @@
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    # retaurant urls
+    # restaurant urls
     path('', views.show_restaurants),
-    #path('new/', views.new_restaurants),
-    path('edit/<int:restaurant_id>', views.edit_restaurants),
-    #path('delete/<int:restaurant_id>', views.delete_restaurants),
+    path('new/', views.new_restaurant), 
+    path('edit/<int:restaurant_id>', views.edit_restaurant), 
+    path('delete/<int:restaurant_id>', views.delete_restaurant), 
 
     # menu itens urls
-
-    path('menus/<int:restaurant_id>', views.show_menu_itens, name='menu'),
-    path('menus/<int:restaurant_id>/new/', views.new_menu_item, name='new'),
-    #path('menus/<int:restaurant_id>/edit/<int:item_id>',views.edit_menu_item, name='edit'),
-    #path('menus/<int:restaurant_id>/delete/<int:item_id>',views.delete_menu_item, name='delete'),
-
+    path('menus/<int:restaurant_id>', views.show_menu_itens, name="menu"), 
+    path('menus/<int:restaurant_id>/new/', views.new_menu_item, name="new"),
+    path('menus/<int:restaurant_id>/edit/<int:item_id>/', views.edit_menu_item, name="edit"),
+    path('menus/<int:restaurant_id>/delete/<int:item_id>/', views.delete_menu_item, name="delete"),
 ]
--- menus/views.py
+++ menus/views.py
@@ -7,17 +7,27 @@ from menus.models import MenuItem, Restaurant
 # Autor: Rafael Zeitoune
 # última Alteração: 06/06/2022
 
-# -------------------Views do modelo Restaurant----------------------------------------------
-
-
+# Restaurant views.
 def show_restaurants(request):
-    """ Show restaurants by name """
+    """show restaurants by name."""
     restaurants = Restaurant.objects.order_by('name')
     context = {'restaurants': restaurants}
     return render(request, "restaurants/show_restaurants.html", context)
 
 
-def edit_restaurants(request, restaurant_id):
+def new_restaurant(request):
+    """create a new restaurant."""
+    if request.method == 'POST':
+        form_data = request.POST
+        restaurant = Restaurant()
+        restaurant.name = form_data.get('name')
+        restaurant.save()
+        return redirect("/")
+    else:
+        return render(request, 'restaurants/new_restaurant.html')
+
+
+def edit_restaurant(request, restaurant_id):
     """edit a restaurant by a id."""
     restaurant = Restaurant.objects.get(pk=restaurant_id)
     if request.method == 'POST':
@@ -26,17 +36,23 @@ def edit_restaurants(request, restaurant_id):
         restaurant.save()
         return redirect("/")
     else:
-        return render(request, "restaurants/edit_restaurant.html", {'restaurant': restaurant})
+        return render(request, 'restaurants/edit_restaurant.html', {'restaurant': restaurant})
 
 
-# ----------------Views do modelo MenuItem --------------------------------------------------
+def delete_restaurant(request, restaurant_id):
+    restaurant = Restaurant.objects.get(pk=restaurant_id)
+    if request.method == 'POST':
+        restaurant.delete()
+        return redirect("/")
+    else:
+        return render(request, 'restaurants/delete_restaurant.html', {'restaurant': restaurant})
 
 
+# MenuItem views
 def show_menu_itens(request, restaurant_id):
-    """ Show a menus by category for a restaurant """
-    menu_itens = MenuItem.objects.filter(
-        restaurant_id=restaurant_id).order_by('category')
-    context = {'restaurants': restaurant_id, 'menu_itens': menu_itens}
+    """show a menus by category for a restaurant"""
+    menu_itens = MenuItem.objects.order_by('category')
+    context = {'restaurant_id':restaurant_id, 'menu_itens': menu_itens}
     return render(request, "menus/show_menu_itens.html", context)
 
 
@@ -54,3 +70,27 @@ def new_menu_item(request, restaurant_id):
         return redirect(f"/menus/{restaurant_id}")
     else:
         return render(request, 'menus/new_menu_item.html', {'restaurant_id': restaurant_id})
+
+
+def edit_menu_item(request, restaurant_id, item_id):
+    """edit a menu item by a id."""
+    menu_item = MenuItem.objects.get(pk=item_id)
+    if request.method == 'POST':
+        form_data = request.POST
+        menu_item.category = form_data.get('category')
+        menu_item.name = form_data.get('name')
+        menu_item.description = form_data.get('description')
+        menu_item.price = form_data.get('price')
+        menu_item.save()
+        return redirect(f"/menus/{restaurant_id}")
+    else:
+        return render(request, 'menus/edit_menu_item.html', {'menu_item': menu_item})
+
+
+def delete_menu_item(request, restaurant_id, item_id):
+    menu_item = MenuItem.objects.get(pk=item_id)
+    if request.method == 'POST':
+        menu_item.delete()
+        return redirect(f"/menus/{restaurant_id}")
+    else:
+        return render(request, 'menus/delete_menu_item.html', {'menu_item': menu_item})
